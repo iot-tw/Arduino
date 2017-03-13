b9cc097fd28852185cf7c66c03d3b715314b06d8
Marty Chao <marty@browan.com>
2017-03-13 17:48:29 +0800

    delete AT-DTX Lab01

--- Lab04_giot_VariableResistor_Button/Lab04_giot_VariableResistor_Button.ino
+++ Lab04_giot_VariableResistor_Button/Lab04_giot_VariableResistor_Button.ino
@@ -1,15 +1,16 @@
-//讀取A0 電壓，讀取 Pin 4 High/Low ,LoRa 發送出去
+//讀取A0 電壓，讀取 Pin 4 High,beep PIN 9，/Low ,LoRa 發送出去
 #include <SoftwareSerial.h>
 
 SoftwareSerial LoRaUART(10, 11); // to TX, to RX
 const int variable = A0;
-const int led = 9;
+const int beep = 9;
 const int button = 4;
 int btVoltage = 0;
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(4, INPUT);
+  pinMode(button, INPUT);
+  pinMode(beep, OUTPUT);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -27,20 +28,23 @@ void setup() {
 void loop() {
   int sensorValue = analogRead(variable);
   String sensorData;
-  sensorData = String(sensorValue,4);
+  sensorData = String(sensorValue);
   Serial.println(sensorData);
 
   btVoltage = digitalRead(button);
 
   if (btVoltage == HIGH){
     digitalWrite(LED_BUILTIN, HIGH);
-    //Serial.println("Push down");
+    tone(beep, 262, 200);
+    Serial.println("Push down");
     Serial.println("AT+DTX=" + String(sensorData.length()+1) + ",\"" + "B" + sensorData + "\"");
     LoRaUART.println("AT+DTX=" + String(sensorData.length()+1) + ",\"" + "B" + sensorData + "\"");
   }
   else{
     digitalWrite(LED_BUILTIN, LOW);
-    //Serial.println("Button up");
+    digitalWrite(9, LOW);
+    Serial.println("Button up");
+    tone(beep, sensorValue, 200);
     Serial.println("AT+DTX=" + String(sensorData.length()+1) + ",\"" + "R" + sensorData + "\"");
     LoRaUART.println("AT+DTX=" + String(sensorData.length()+1) + ",\"" + "R" + sensorData + "\"");
   }

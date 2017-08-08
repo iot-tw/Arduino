710452d986fe391f62800a847d9f43271b822cc9
Marty Chao <marty@browan.com>
2017-08-08 11:34:30 +0800

    Init version: Use Max485 to connect ModBus Slave Device first 4 Registers, And Print to Serial Console.

---
+++ Lab11_Max485_ModBus_Master_Reader/Lab11_Max485_ModBus_Master_Reader.ino
@@ -0,0 +1,100 @@
+/*
+
+  RS485_HalfDuplex.pde - example using ModbusMaster library to communicate
+  with EPSolar LS2024B controller using a half-duplex RS485 transceiver.
+
+  This example is tested against an EPSolar LS2024B solar charge controller.
+  See here for protocol specs:
+  http://www.solar-elektro.cz/data/dokumenty/1733_modbus_protocol.pdf
+
+  Library:: ModbusMaster
+  Author:: Marius Kintel <marius at kintel dot net>
+
+  Copyright:: 2009-2016 Doc Walker
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+#include <ModbusMaster.h>
+
+/*!
+  We're using a MAX485-compatible RS485 Transceiver.
+  Rx/Tx is hooked up to the hardware serial port at 'Serial'.
+  The Data Enable and Receiver Enable pins are hooked up as follows:
+*/
+
+//in my case,i let DE & RE jump together to Pin 3
+#define MAX485_DE      3
+#define MAX485_RE_NEG  3
+
+// instantiate ModbusMaster object
+ModbusMaster node;
+
+void preTransmission()
+{
+  digitalWrite(MAX485_RE_NEG, 1);
+  digitalWrite(MAX485_DE, 1);
+}
+
+void postTransmission()
+{
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 0);
+}
+
+void setup()
+{
+  pinMode(MAX485_RE_NEG, OUTPUT);
+  pinMode(MAX485_DE, OUTPUT);
+  // Init in receive mode
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 0);
+
+  // Modbus communication runs at 9600 baud
+  Serial.begin(9600);
+
+  // Modbus slave ID 1 
+  node.begin(1, Serial);
+
+  
+  // Callbacks allow us to configure the RS485 transceiver correctly
+  node.preTransmission(preTransmission);
+  node.postTransmission(postTransmission);
+}
+
+bool state = true;
+
+void loop()
+{
+  uint8_t result;
+  uint16_t data[6];
+  
+  
+
+  // Read 4 registers starting at 0x0000)
+  result = node.readHoldingRegisters(0x0000, 4);
+  if (result == node.ku8MBSuccess)
+  {
+    Serial.println("SlaveValue:"); 
+    Serial.println(node.getResponseBuffer(0x00));
+    Serial.println(node.getResponseBuffer(0x01));
+    Serial.println(node.getResponseBuffer(0x02));
+    Serial.println(node.getResponseBuffer(0x03));
+     
+  }
+
+  delay(1000);
+}
+
+
